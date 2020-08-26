0b2c174c8509bc276232d04145b29f8ac42dceb6
slurio <samantha.lurio@gmail.com>
2020-08-25 21:41:47 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails'
+
+gem 'binding_of_caller', '~> 0.8.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~>1.3.6'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :students, except: [:destory]
+  resources :school_classes, except: [:destroy]
+  
+  # get "/students", to: "students#index", as: "students"
+  # get "/students/new", to: "students#new"
+  # get "/students/:id/edit", to: "students#edit"
+  # get "/students/:id", to: "students#show", as: "student"
+  # post "/students", to: "students#create"
+  # patch "/students/:id", to: "students#update"
+  
 end
